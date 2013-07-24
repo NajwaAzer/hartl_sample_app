1c1572b858515ed638a87855f02457e1bf5760c4
NajwaAzer <najwa.azer@gmail.com>
2013-07-24 00:18:59 +0000

    Change to Rails 4.0 code

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '2f48610be7cc28ee7870a66f9806474ac99f1f800ae390a21e5c9f1d2384647616b605ead699c58a95109941959d05fe44a8c5c5dc5d9c091b243b7506bd4325'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
